6f501172284791118513b46048c12935df102db8
Jason Hegarty <jason@jheg.co.uk>
2024-08-08 08:59:52 +0100

    rpsls refactoriing

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -1,138 +1,132 @@
-"""
-Both opponents choose an item from rock, paper, or scissors. The winner is 
-decided according to the following rules:
-
-If player a chooses rock and player b chooses scissors, player a wins.
-If player a chooses paper and player b chooses rock, player a wins.
-If player a chooses scissors and player b chooses paper, player a wins.
-If both players choose the same item, neither player wins. It's a tie.
-Our version of the game lets the user play against the computer. The game flow 
-should go like this:
-
-The user makes a choice.
-The computer makes a choice.
-The winner is displayed.
-"""
-# X Extract messages to a dictionary
-# X Simplify input choices with a single key input from ['1','2','3']
-# X Build best of 5
-# X Extend with Spock and Lizard options
-
 import random
+import time
 
 VALID_CHOICES = {
-    '1': 'Rock',
-    '2': 'Paper',
-    '3': 'Scissors',
-    '4': 'Lizard',
-    '5': 'Spock'
+    '1': 'rock',
+    '2': 'paper',
+    '3': 'scissors',
+    '4': 'lizard',
+    '5': 'spock'
 }
-formatted_choices = [f"{value} ({key})" for key, value in VALID_CHOICES.items()]
+
+WINNING_COMBOS = {
+    'rock': {
+        'lizard': 'crushes',
+        'scissors': 'crushes',
+        },
+    'paper': {
+        'rock': 'covers',
+        'spock': 'disproves',
+        },
+    'scissors': {
+        'paper': 'cuts',
+        'lizard': 'decapitates',
+        },
+    'lizard': {
+        'paper': 'eats',
+        'spock': 'poisons',
+        },
+    'spock': {
+        'scissors': 'smashes',
+        'rock': 'vaporizes',
+        }
+}
+WELCOME = """
+Welcome to Rock, Paper, Scissors, Lizard, Spock.
+
+Options: 
+1 => Rock (crushes lizard & scissors)
+2 => Paper (covers rock & disproves spock)
+3 => Scissors (cuts paper & decapitates lizard)
+4 => Lizard (eats paper & poisons spock)
+5 => Spock (smashes scissors & vaporizes rock)
+
+Best of 5, good luck!
+"""
+MESSAGES = {
+    "winner":               "You win!",
+    "lose":                 "You lose",
+    "tie":                  "It's a tie\n",
+    "end_winner":           "You are the winner!",
+    "end_loser":            "Game over, you lose.",
+    "play_again":           "Play again? (y/n)"
+}
+
+formatted_choices = [
+    f"{value} ({key})" for key, value in VALID_CHOICES.items()
+]
 
 def invalid_choice(choice):
-    if choice not in VALID_CHOICES:
+    if choice not in VALID_CHOICES.keys():
         return True
     return False
 
 def prompt(msg):
     print(f"==> {msg}")
 
-messages = {
-    "border":               "+----------------------------------+",
-    "welcome":              "| Welcome to Rock, Paper, Scissors",
-    "intro":                "| Best of 5",
-    "new_high_score":       "| NEW HIGH SCORE",
-    "winner":               "You win!",
-    "lose":                 "You lose",
-    "tie":                  "It's a tie",
-    "play_again":           "Play again? (y/n)"
-}
-
 def update_score(number):
     return number + 1
 
-def get_winner(player_1_choice, player_2_choice):
-    match player_1_choice:
-        case '1':
-            if player_2_choice in ['3', '4']:
-                return 'player_1'
-            if player_2_choice in ['2', '5']:
-                return 'player_2'
-        case '2':
-            if player_2_choice in ['1', '5']:
-                return 'player_1'
-            if player_2_choice in ['3', '4']:
-                return 'player_2'
-        case '3':
-            if player_2_choice in ['2', '4']:
-                return 'player_1'
-            if player_2_choice in ['1', '5']:
-                return 'player_2'
-        case '4':
-            if player_2_choice in ['2', '5']:
-                return 'player_1'
-            if player_2_choice in ['1', '3']:
-                return 'player_2'
-        case '5':
-            if player_2_choice in ['1', '3']:
-                return 'player_1'
-            if player_2_choice in ['2', '4']:
-                return 'player_2'
+def player_wins(player_1_choice, player_2_choice):
+    return player_2_choice in WINNING_COMBOS[player_1_choice].keys()
 
-    return 'tie'
+def display_outcome(winner, loser):
+    prompt(f'Outcome:{" " * 12}{VALID_CHOICES[winner]} '
+           f'{WINNING_COMBOS[VALID_CHOICES[winner]][VALID_CHOICES[loser]]} '
+           f'{VALID_CHOICES[loser]}\n')
 
 while True:
-    prompt(messages['border'])
-    prompt(messages['welcome'])
-    prompt(messages['intro'])
-    prompt(messages['border'])
+    prompt(WELCOME)
 
     player_1_score = 0
     player_2_score = 0
+    game_round = 1
 
     while True:
-        # prompt(f'| Highest Win Streak: {highest_win_streak}')
-        print(f"Player 1 score: {player_1_score}")
-        print(f"Player 2 score: {player_2_score}")
-        prompt('')
+        time.sleep(1)
+        prompt(f'Round {game_round}')
+        if game_round > 1:
+            prompt(f"Player 1 score: {player_1_score}")
+            prompt(f"Computer score: {player_2_score}\n")
 
-        prompt(f'| Choose: {", ".join(formatted_choices)}')
-        player_choice = input('Enter your choice: ')
+        # prompt(f'| Choose: {", ".join(formatted_choices)}')
+        prompt('Enter your choice:')
+        player_choice = input()
         computer_choice = random.choice(list(VALID_CHOICES.keys()))
-        prompt('')
 
         while invalid_choice(player_choice):
-            prompt(f'| Please select from the following: '
+            prompt(f'Please select from the following: '
                     f'{", ".join(formatted_choices)}')
             player_choice = input('Enter your choice: ')
-            prompt('')
 
-        prompt(f'| Your choice: {VALID_CHOICES[player_choice]}')
-        prompt(f'| Computers choice: {VALID_CHOICES[computer_choice]}')
+        player_choice_string = VALID_CHOICES[player_choice]
+        computer_choice_string = VALID_CHOICES[computer_choice]
 
-        get_winner(player_choice, computer_choice)
+        prompt(f'You: {" " * 15}{player_choice_string.capitalize()}')
+        prompt(f'Computer: {" " * 10}{computer_choice_string.capitalize()}')
 
-        if get_winner(player_choice, computer_choice) == 'player_1':
-            prompt(f'WINNER {VALID_CHOICES[player_choice]} '
-                   f'beats {VALID_CHOICES[computer_choice]}')
+        if player_wins(player_choice_string, computer_choice_string):
             player_1_score = update_score(player_1_score)
-        elif get_winner(player_choice, computer_choice) == 'player_2':
-            prompt(f'LOSER {VALID_CHOICES[computer_choice]} '
-                   f'beats {VALID_CHOICES[player_choice]}')
+            prompt(f'Result:{" " * 13}{MESSAGES["winner"]}')
+            display_outcome(player_choice, computer_choice)
+        elif player_wins(computer_choice_string, player_choice_string):
             player_2_score = update_score(player_2_score)
+            prompt(f'Result:{" " * 13}{MESSAGES["lose"]}')
+            display_outcome(computer_choice, player_choice)
         else:
-            prompt("It's a tie")
+            prompt(f'Result:{" " * 13}{MESSAGES["tie"]}')
 
-        prompt('')
+        game_round += 1
 
         if player_1_score == 3:
-            prompt('You are the winner!')
+            prompt(MESSAGES["end_winner"])
             break
         if player_2_score == 3:
-            prompt('Game over, you lose.')
+            prompt(MESSAGES["end_loser"])
             break
 
-    prompt(messages['play_again'])
+
+    prompt(MESSAGES['play_again'])
     play_again = input()
     if play_again == 'n':
         break
